f692f5cc343d21ccd64a8eaed567905d24b878e6
zir31 <redstorm31rus@gmail.com>
2022-09-22 14:05:26 +0300

    Add project files.

---
+++ SeriesWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SeriesWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
