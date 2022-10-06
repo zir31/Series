e3aae2d30615f8557f5f48ddb31d0282ff44849c
zir31 <redstorm31rus@gmail.com>
2022-10-06 16:55:32 +0300

    Added View for Category, installed bootstrap icons

---
+++ SeriesWeb/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeriesWeb.Data;
+using SeriesWeb.Models;
+
+namespace SeriesWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
